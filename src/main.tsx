6e985b359fdd1475b549185bcb56123edbe98b88
Роман Кондрашев <distanceon@MacBook-Air-Roman.local>
2023-11-08 20:43:19 +0300

    solve problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './app/App.tsx'
-import { Providers } from './app/providers/Providers.tsx'
+import App from './app/App'
+import { Providers } from './app/providers/Providers'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
